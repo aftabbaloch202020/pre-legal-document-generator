abb8f372933f6659e54f4ed1e1f4fa360dbf2d7a
Aftab Baloch <aftabbaloch.202020@gmail.com>
2026-09-14 13:24:55 +0500

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,31 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: {
+          50: "#f0fdf4",
+          100: "#dcfce7",
+          500: "#16a34a",
+          600: "#15803d",
+          700: "#166534",
+        },
+        navy: {
+          800: "#1e293b",
+          900: "#0f172a",
+          950: "#020617",
+        }
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
